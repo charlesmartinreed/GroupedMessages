76aa835b25e476c116ad836df76c391757156a95
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-03 16:56:13 -0600

    Layout chat bubbles in custom table view cell.

--- GroupedMessages/ChatMessageCell.swift
+++ GroupedMessages/ChatMessageCell.swift
@@ -11,21 +11,38 @@ import UIKit
 class ChatMessageCell: UITableViewCell {
     
     let messageLabel = UILabel() //represents a text message, add to cell to make visible
+    let bubbleBackgroundView = UIView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        //MARK:- Background view setup
+        bubbleBackgroundView.backgroundColor = .yellow
+        bubbleBackgroundView.layer.cornerRadius = 10
+        bubbleBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(bubbleBackgroundView)
+        
         addSubview(messageLabel)
         messageLabel.text = "This is a long string that should wrap onto the next line. If we do this correctly, that is. Maybe even wrap onto the third line if I do this correctly and blather on for far longer than I need to."
         messageLabel.numberOfLines = 0
-        messageLabel.backgroundColor = .green
+        //messageLabel.backgroundColor = .green
         messageLabel.translatesAutoresizingMaskIntoConstraints = false
         
         //MARK:- autolayout constraints for our label
-        let constraints = [messageLabel.topAnchor.constraint(equalTo: topAnchor),
-        messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
-        messageLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
-        messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor)]
+        //larger constants on the messageLabel constraints pushes the conent further into the cell, increasing perceived distance between individual cells
+        let constraints =
+            [
+            messageLabel.topAnchor.constraint(equalTo: topAnchor, constant: 32),
+            messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            messageLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -32),
+            messageLabel.widthAnchor.constraint(equalToConstant: 250),
+        //MARK:- autolayout constraints for our background view
+        //after adding subview of messageLabel because they are derived from messageLabel
+            bubbleBackgroundView.topAnchor.constraint(equalTo: messageLabel.topAnchor, constant: -16),
+            bubbleBackgroundView.leadingAnchor.constraint(equalTo: messageLabel.leadingAnchor, constant: -16),
+            bubbleBackgroundView.trailingAnchor.constraint(equalTo: messageLabel.trailingAnchor, constant: 16),
+            bubbleBackgroundView.bottomAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 16)
+        ]
         
         NSLayoutConstraint.activate(constraints)
     }
--- GroupedMessages/ViewController.swift
+++ GroupedMessages/ViewController.swift
@@ -11,6 +11,13 @@ import UIKit
 class ViewController: UITableViewController {
 
     private let cellId = "id123"
+    let textMessages = [
+        "Here's my very first message",
+        "I'm going to message another long message that should word wrap. Pretty sure lorem ipusum would've been a good implementation for this tutorial, but maybe that's no cool anymore?",
+        "Here's my very first message",
+        "I'm going to message another long message that should word wrap. Pretty sure lorem ipusum would've been a good implementation for this tutorial, but maybe that's no cool anymore? Here's my very first message I'm going to message another long message that should word wrap. Pretty sure lorem ipusum would've been a good implementation for this tutorial, but maybe that's no cool anymore?",
+        "One last message for the road. Thanks for that!"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,21 +28,21 @@ class ViewController: UITableViewController {
         //registering our cell for dequeuing
         //registering our ChatMessageCell instead of the regular UITableViewCell
         tableView.register(ChatMessageCell.self, forCellReuseIdentifier: cellId)
+        tableView.separatorStyle = .none
         
     }
     
     //MARK:- Delegate methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 10
+        return textMessages.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ChatMessageCell
 //        cell.textLabel?.text = "This is a long string that should wrap onto the next line. If we do this correctly, that is. Maybe even wrap onto the third line if I do this correctly and blather on for far longer than I need to."
-        cell.textLabel?.numberOfLines = 0 //pushes the text down, iOS handles the sizing automagically
+        cell.messageLabel.text = textMessages[indexPath.row]
         return cell
     }
 
